90ecba7940273a6ff15c6087916b537c105f3250
mjl371 <jialemiao371@gmail.com>
2025-03-14 00:26:34 +0800

    ui美化

--- src/main/java/com/einvoice/gui/InvoiceController.java
+++ src/main/java/com/einvoice/gui/InvoiceController.java
@@ -25,6 +25,7 @@ import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.control.SelectionMode;
+import javafx.scene.control.TableCell;
 import javafx.scene.control.Label;
 import javafx.stage.DirectoryChooser;
 import javafx.stage.FileChooser;
@@ -82,8 +83,35 @@ public class InvoiceController implements Initializable {
         buyerNameColumn.setCellValueFactory(new PropertyValueFactory<>("buyerName"));
         amountColumn.setCellValueFactory(new PropertyValueFactory<>("amount"));
 
-        tableView.setItems(invoiceList);
+        // 设置列宽度自适应内容
+        setColumnWidthAutoAdjust(fileNameColumn);
+        setColumnWidthAutoAdjust(titleColumn);
+        setColumnWidthAutoAdjust(machineNumberColumn);
+        setColumnWidthAutoAdjust(codeColumn);
+        setColumnWidthAutoAdjust(numberColumn);
+        setColumnWidthAutoAdjust(dateColumn);
+        setColumnWidthAutoAdjust(buyerNameColumn);
+        setColumnWidthAutoAdjust(amountColumn);
 
+        tableView.setItems(invoiceList);
+    }
+
+    private <T> void setColumnWidthAutoAdjust(TableColumn<InvoiceWrapper, T> column) {
+        column.setCellFactory(tc -> {
+            TableCell<InvoiceWrapper, T> cell = new TableCell<InvoiceWrapper, T>() {
+                @Override
+                protected void updateItem(T item, boolean empty) {
+                    super.updateItem(item, empty);
+                    if (item != null) {
+                        setText(item.toString());
+                    } else {
+                        setText(null);
+                    }
+                }
+            };
+            cell.setPrefWidth(TableCell.USE_COMPUTED_SIZE);
+            return cell;
+        });
     }
 
     @FXML
